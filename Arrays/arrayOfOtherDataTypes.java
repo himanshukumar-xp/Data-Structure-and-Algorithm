7dd79090264bafa854585f89107f927bfbecef8c
Himanshu Kumar <hp74473@gmail.com>
2025-08-26 20:12:53 +0530

    Add files via upload

---
+++ Arrays/arrayOfOtherDataTypes.java
@@ -0,0 +1,8 @@
+public class arrayOfOtherDataTypes {
+    public static void main(String[] args) {
+        char[] arr = new char[8];
+        for(int i =0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
