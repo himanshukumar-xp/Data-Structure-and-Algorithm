93e3ef8b8e9ed12cec4cd9074f8cfac984e708b1
Himanshu Kumar <hp74473@gmail.com>
2025-08-28 22:07:17 +0530

    Add files via upload

---
+++ Arrays/Segregate_0_and_1s.java
@@ -0,0 +1,19 @@
+public class Segregate_0_and_1s {
+    public static void main(String[] args) {
+        int[] arr = {0,1,0,0,1,1,1,0,1,0,1,1,1,0};
+        int n = arr.length;
+        int i =0, j= n-1;
+        while(i<j){
+            if (arr[i] == 0) i++ ;
+            else if (arr[j] == 1) j--; 
+             else if (arr[i] ==1 && arr[j] == 0) {
+                arr[i] = 0;
+                arr[j] = 1;
+                i++;
+                j--;
+             }       
+            
+        }
+        System.out.print(arr[i]+" ");
+    }
+}
