20388dbe3b78050f237bd0e6a77a30eadef2e161
Himanshu Kumar <hp74473@gmail.com>
2025-09-06 22:54:42 +0530

    Add files via upload

---
+++ Strings/CompareToString.java
@@ -0,0 +1,14 @@
+public class CompareToString {
+    
+    public static void main(String[] args) {
+        String a = "Himanshu";
+        String b = "Harshit";
+        // System.out.println(a.compareTo(b));
+        // System.out.println(a.concat(b));
+        //or
+        // System.out.println(a = a+b);
+        // System.out.println("Himanshu"+10+20); // Himanshu1020
+        System.out.println(10+20+"Himanshu");
+        System.out.println(10+"Himanshu"+20);
+    }
+}
