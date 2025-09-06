b14f45914a303e4b30c7fdabe594c3896198e6f1
Himanshu Kumar <hp74473@gmail.com>
2025-09-06 22:55:02 +0530

    Add files via upload

---
+++ Strings/Built_In_Method.java
@@ -0,0 +1,9 @@
+public class Built_In_Method {
+  public static void main(String[] args) {
+    String s = "Himanshu Pandey";
+    // System.out.println(s.toUpperCase());
+    System.out.println(s.contains("anshu"));
+    
+  } 
+}
+ 
