95a35891a01b8fd559b29bba8072a1c6ddc2024a
Himanshu Kumar <hp74473@gmail.com>
2025-09-07 22:32:38 +0530

    Add files via upload

---
+++ Strings/StrToCharArray.java
@@ -0,0 +1,10 @@
+public class StrToCharArray {
+    
+    public static void main(String[] args) {
+        String s = "Himanshu";
+        char[] arr = s.toCharArray();
+        for(char ch: arr){
+            System.out.print(ch);
+        }
+    }
+}
