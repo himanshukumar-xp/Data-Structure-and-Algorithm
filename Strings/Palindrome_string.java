4e9e3687eee8da9ef2541ea7fbf8cdde4d70e68d
Himanshu Kumar <hp74473@gmail.com>
2025-09-10 22:40:40 +0530

    Add files via upload

---
+++ Strings/Palindrome_string.java
@@ -0,0 +1,16 @@
+public class Palindrome_string {
+    public static boolean isPalindrome(String str){
+        int n = str.length();
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(n-1-i)){
+              return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        String str = "racec";
+        System.out.println(isPalindrome(str));
+    }
+}
